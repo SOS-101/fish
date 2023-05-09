85315bca5439e1c5695189302f38b2300886c1bd
ooohpi <taochangwan@qq.com>
2023-05-09 16:42:48 +0800

    set basic config

--- config.fish
+++ config.fish
@@ -1,26 +1,10 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
-  set -x XMODIFIERS "@im=fcitx"
-  set -x GTK_IM_MODULE fcitx
-  set -x QT_IM_MODULE fcitx
-  set -x DWM "/home/ooohpi/workspace/dwm"
-  set -x PATH "/home/ooohpi/.local/bin" $PATH
+  source $FISH_CONFIG_DIR/abbr/abbrs.fish
+end
 
-  theme_gruvbox dark medium
-
-  fishx
-
-  abbr --add cg chatgpt
-  abbr --add q exit
-  abbr --add c clear
-  abbr --add nb newsboat
-  abbr --add lg lazygit
-  abbr --add wifil "nmcli device wifi list"
-  abbr --add wific "nmcli device wifi connect"
-  abbr --add z zathura
-  abbr --add mv "mv -v"
-  abbr --add cp "cp -v"
-  abbr --add rm "rm -v"
-  abbr --add cat "bat --style plain"
-  abbr --add sn "shutdown now"
+if status is-login
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    exec startx -- -keeptty &> /dev/null
+  end
 end
